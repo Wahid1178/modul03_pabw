0bc15c2ed49a67d138ac5286180b88fabcfe0141
Nama Kamu <emailkamu@example.com>
2025-10-07 11:36:05 +0700

    first commit

---
+++ resources/views/form.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+    <title>Form biodata</title>
+</head>
+<body>
+    <h2>Input Biodata</h2>
+    <form action="/proses" method="post">
+
+    @csrf
+
+    <label>Nama</label>
+    <input type="text" name="nama"> <br><br>
+
+    <label>Umur</label>
+    <input type="number" name="umur"> <br><br>
+
+    <label>Alamat</label>
+    <textarea name="alamat"></textarea><br><br>
+
+    <button type="submit">kirim</button>
+</form>
+</body>
+</html>
---
+++ resources/views/hasil.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hasil Biodata</title>
+</head>
+<body>
+    <h2>Data Biodata</h2>
+    <p><strong>Nama:</strong>{{$nama}}</p>
+    <p><strong>umur:</strong>{{$umur}}</p>
+    <p><strong>Alamat:</strong>{{$alamat}}</p>
+</body>
+</html>
